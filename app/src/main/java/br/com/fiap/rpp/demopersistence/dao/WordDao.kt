f13ec0727db38d7b8ad82392f222945547598e75
Logon Aluno <logonaluno@local.com>
2020-03-02 20:59:31 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/rpp/demopersistence/dao/WordDao.kt
@@ -0,0 +1,21 @@
+package br.com.fiap.rpp.demopersistence.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import br.com.fiap.rpp.demopersistence.entity.Word
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): LiveData<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
---
+++ app/src/main/java/br/com/fiap/rpp/demopersistence/ui/main/MainActivity.kt
@@ -0,0 +1,70 @@
+package br.com.fiap.rpp.demopersistence.ui.main
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.fiap.rpp.demopersistence.R
+import br.com.fiap.rpp.demopersistence.entity.Word
+import br.com.fiap.rpp.demopersistence.ui.newword.NewWordActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var wordViewModel: WordViewModel
+
+    private lateinit var adapter: WordListAdapter
+
+    private val newWordActivityRequestCode = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        wordViewModel = ViewModelProvider(this).get(WordViewModel::class.java)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewWordActivity::class.java)
+            startActivityForResult(intent, newWordActivityRequestCode)
+        }
+
+        this.observer()
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == newWordActivityRequestCode && resultCode == Activity.RESULT_OK) {
+            data?.getStringExtra(NewWordActivity.EXTRA_REPLY)?.let {
+                val word = Word(it)
+                wordViewModel.insert(word)
+            }
+        } else {
+            Toast.makeText(
+                applicationContext,
+                R.string.empty_not_saved,
+                Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun observer() {
+
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.setWords(it) }
+        })
+    }
+
+}
